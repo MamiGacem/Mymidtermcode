f0e592e90747d9d10d1d7cc594728b76c262f2d2
MamiGacem <mahdirayan712@gmail.com>
2023-04-11 14:50:12 -0400

    design

--- src/design/UseEmployee.java
+++ src/design/UseEmployee.java
@@ -1,4 +1,11 @@
 package design;
 
 abstract class UseEmployee {
+    public abstract void EmployeeInfo(); // this is abstract method without body
+    // to use this abstarct method i need to extends it in toyota
+// non abstract method
+    public void EmployeeId(){
+        System.out.println("empolyee id ");
+    }
 }
+
